4d660a29e08b3d3f9b171891c783b7783ec11221
TalitaFraga <talitad.fraga@gmail.com>
2020-12-03 21:37:16 -0300

    correção app

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose')
 
 const app = express()
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(
+    `${process.env.MONGODB_URL}`, {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 })
